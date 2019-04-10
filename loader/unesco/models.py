4478218122db05cf96f55d5be4a6a62643944204
Will Godley <wmgodley@umich.edu>
2019-04-10 01:43:31 +0000

    ready to start inserting

--- loader/unesco/models.py
+++ loader/unesco/models.py
@@ -1,78 +1,49 @@
 from django.db import models
 
 class Category(models.Model) :
-    name = models.CharField(max_length=128)
+    category = models.CharField(max_length=128)
 
     def __str__(self) :
-        return self.name
+        return self.category
 
-class Category(models.Model) :
-    name = models.CharField(max_length=128)
-
-    def __str__(self) :
-        return self.name
-
-class Category(models.Model) :
-    name = models.CharField(max_length=128)
+class States(models.Model) :
+    state = models.CharField(max_length=128)
+    category = models.ManyToManyField('Category', through = 'Site')
+    region = models.ManyToManyField('Region', through = 'Site')
 
     def __str__(self) :
-        return self.name
+        return self.state
 
-class Category(models.Model) :
-    name = models.CharField(max_length=128)
-
-    def __str__(self) :
-        return self.name
-
-class Category(models.Model) :
-    name = models.CharField(max_length=128)
+class Region(models.Model) :
+    region = models.CharField(max_length=128)
+    category = models.ManyToManyField('Category', through = 'Site')
+    states = models.ManyToManyField('States', through = 'Site')
 
     def __str__(self) :
-        return self.name
+        return self.region
 
-class Category(models.Model) :
-    name = models.CharField(max_length=128)
-
-    def __str__(self) :
-        return self.name
-
-class Category(models.Model) :
-    name = models.CharField(max_length=128)
+class Iso(models.Model) :
+    iso = models.CharField(max_length=128)
 
     def __str__(self) :
-        return self.name
-
-class Category(models.Model) :
-    name = models.CharField(max_length=128)
-
-    def __str__(self) :
-        return self.name
-
-class Category(models.Model) :
-    name = models.CharField(max_length=128)
-
-    def __str__(self) :
-        return self.name
-
-class Category(models.Model) :
-    name = models.CharField(max_length=128)
-
-    def __str__(self) :
-        return self.name
-
-class Category(models.Model) :
-    name = models.CharField(max_length=128)
-
-    def __str__(self) :
-        return self.name
+        return self.iso
 
 
 class Site(models.Model):
     name = models.CharField(max_length=128)
     year = models.IntegerField(null=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
-    ....
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    states = models.ForeignKey(States, on_delete=models.CASCADE)
+    region = models.ForeignKey(Region, on_delete=models.CASCADE)
+    iso = models.ForeignKey(Iso, on_delete=models.CASCADE)
+
+    description = models.TextField()
+    justification = models.TextField()
+    longitude = models.IntegerField(null=True)
+    latitude = models.IntegerField(null=True)
+    area_hectares = models.IntegerField(null=True)
+
 
     def __str__(self) :
-        return self.
+        return self.name
